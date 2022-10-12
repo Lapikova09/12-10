472a2169ed72ad4866c8406a420f70c6dc085d87
Lapikova09 <96071466+Lapikova09@users.noreply.github.com>
2022-10-12 18:29:08 +0300

    Add files via upload

---
+++ 12102022.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	/*//For1. Даны целые числа K и N (N > 0). Вывести N раз число K.
+	//int n, k;
+	//cin >> n>> k;
+	//for (int i = 0; i < n; ++i) {
+	//	cout << k << endl;
+	//}
+	//system("pause");*/
+
+
+	/*//For2. Даны два целых числа A и B (A < B). Вывести в порядке возрастания все
+	//целые числа, расположенные между A и B(включая сами числа A и B), а
+	//	также количество N этих чисел.	//int a, b;	//cin >> a >> b;	//int n = 0;	//for(int i=a; i<=b; i=i+1)	//{	//	cout << i << " ";	//	n++;	//}	//cout << n << endl;
+	//system("pause");*/
+
+
+	/*//For3. Даны два целых числа A и B (A < B). Вывести в порядке убывания все
+	//целые числа, расположенные между A и B(не включая числа A и B), а
+	//	также количество N этих чисел.*/	/*int a, b;	cin >> a >> b;	int n = 0;	for(int i=b-1; i<a; i=i-1)	{		cout << i << " ";		n++;	}	cout << n << endl;
+	system("pause");*/	/*//For4.Дано вещественное число — цена 1 кг конфет.Вывести стоимость 1,
+    //2, . . ., 10 кг конфет.*/   //  double p;	//cout << "p: ";	//cin >> p;	//for (int w = 1; w <= 10; w++) {
+	//	cout << w << "kg costs " << w * p << " $" << endl;
+	//}
+	//system("pause");
+
+	/*For10. Дано целое число N (> 0). Найти сумму
+1 + 1/2 + 1/3 + . . . + 1/N*/
+	/*int n;
+	cin >> n;
+	for(double i=1; i<=n; i++)
+	{
+		cout << 1 / i << endl;
+	}
+	system("pause");*/
+
+
+	/*For5◦
+. Дано вещественное число — цена 1 кг конфет. Вывести стоимость 0.1,
+0.2, . . . , 1 кг конфет.*/
+	/*double p;
+	cout << "p: ";
+	cin >> p;
+	for(double w=0.1;w<=1;w=w+0.1)
+	{
+		cout << w << "kg costs " << w*p << " $" << endl;
+	}
+	system("pause");*/
+
+
+	/*For6. Дано вещественное число — цена 1 кг конфет. Вывести стоимость 1.2,
+1.4, . . . , 2 кг конфет.*/
+	/*double p;
+	cout << "p: ";
+	cin >> p;
+	for (double w = 1.2; w <= 2; w = w + 0.2)
+	{
+		cout << w << "kg costs " << w*p << " $" << endl;
+	}
+	system("pause");*/
+
+
+
+	/*/*For7. Даны два целых числа A и B (A < B). Найти сумму всех целых чисел
+от A до B включительно.*/
+	/*int a, b;	cin >> a >> b;	int s = 0;	for(int i=a; i<=b; i=i+1)	{		s = s + i;			}	cout << s << endl;
+	system("pause");*/
+
+
+	/*For8. Даны два целых числа A и B (A < B). Найти произведение всех целых
+чисел от A до B включительно*/
+    /*int a, b;    cin >> a >> b;    int p = 1;    for (int i = a; i <= b; i = i + 1)    {	    p = p * i;     }     cout << p << endl;
+    system("pause");*/
+
+
+   /*For9. Даны два целых числа A и B (A < B). Найти сумму квадратов всех целых
+чисел от A до B включительно.*/
+   /*  int a, b;     cin >> a >> b;     int s = 0;     for (int i = a; i <= b; i = i + 1)	 {		  s = s + i*i;     }    cout << s << endl;
+    system("pause");*/
+
+
+
+    /*For11. Дано целое число N (> 0). Найти сумму (N+0)2 + (N + 1)2 + (N + 2)2 + . . . + (N+N) 2
+     (целое число)*/
+    // int n;
+    // cin >> n;
+	// int s;
+    // for (int i = 0; i <= n; i++)
+	 //{
+	//	 s = s + (i + n)*(i + n);
+	  //  
+     //  }
+	// cout << s << endl;
+    //  system("pause");
+
+
+
+
+}
